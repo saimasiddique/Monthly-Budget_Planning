5540e835eda1d1e05902fd65e2a3bc38c8c3f978
saimasiddique <siddiquesaima16022@gmail.com>
2022-02-17 02:52:46 +0600

    expenses calculation done

---
+++ app.js
@@ -0,0 +1,11 @@
+document.getElementById('calculate-btn').addEventListener('click', function () {
+    const foodCost = document.getElementById('food-cost-amount').value;
+    const rentCost = document.getElementById('rent-amount').value;
+    const clothCost = document.getElementById('clothes-cost-amount').value;
+    const totalExpense = parseFloat(foodCost) + parseFloat(rentCost) + parseFloat(clothCost);
+    document.getElementById('total-expense').innerText = totalExpense;
+
+    const incomeAmount = document.getElementById('income-amount').value;
+    afterBalance = parseFloat(incomeAmount) - totalExpense;
+    document.getElementById('after-balance').innerText = afterBalance;
+})
